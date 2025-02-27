7b7455f41fcb1eccc014d40dcc5667ee87e034ba
MadhusudhanPathak <myspycompanyid@gmail.com>
2025-02-27 21:39:49 +0100

    added articles page to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 export default function Navbar() {
   const links = [
     { href: "https://dhnz.short.gy/zeal", text: "Zanny Zeal!" },
+    { href: "https://dhnz.short.gy/articles", text: "Discussion Articles" },
     { href: "https://dhnz.short.gy/discussion", text: "For Discussion." },
     { href: "https://dhnz.short.gy/about", text: "Who am I?" },
     { href: "https://dhnz.short.gy/connect", text: "Wanna Connect?" },
